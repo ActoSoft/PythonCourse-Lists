03cb0af26611c0d9d6c7b708050c1a08f4c516ef
Dante <quantum120@hotmail.com>
2020-04-12 16:39:15 -0500

    This program i solved it first printing the value that will be received from the keyboard and later putting it in a for loop defining the index variable and finally sending it to print

--- searcher.py
+++ searcher.py
@@ -13,5 +13,26 @@
   'disobedience'
 ]"""
 
+name = input("Input your name: ")
+print("Welcome", name)
+
+words = [
+  'machine',
+  'subset',
+  'trouble',
+  'starting',
+  'matter',
+  'eating',
+  'truth',
+  'disobedience'
+]
+
+search = input("Input the name of the value you want to search: ")
+
+if search in words:
+    index = words.index(search)
+    print(name, "the element exist in the position: ", index)
+else:
+    print(name, "the element does not exist")
 
 
