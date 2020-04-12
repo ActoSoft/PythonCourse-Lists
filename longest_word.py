8e022303cf0e0c704b458b34203e567f6e93e210
Dante <quantum120@hotmail.com>
2020-04-12 18:10:11 -0500

    This program i resolved it using a cycle for

--- longest_word.py
+++ longest_word.py
@@ -13,8 +13,33 @@
 name = input("Input your name: ")
 print("Welcome", name)
 
-words = [ 'mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot']
-longitud = len(words[3])
-print(longitud)
+words = [
+  'mystery',
+  'brother',
+  'aviator',
+  'crocodile',
+  'pearl',
+  'orchard',
+  'crackpot'
+]
+
+def longest_word(words):
+  len_word = []
+
+  for w in words:
+    len_word.append((len(w), w))
+  
+  #print(len_word)
+
+  len_word.sort()
+
+  #print(len_word)
+
+  return len_word[-1][1]
+
+print(name, "the longest word is:", longest_word(words))
+
+
+
 
 
