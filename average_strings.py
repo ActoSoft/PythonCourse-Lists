e6eb7f6051e21426574d6065ca3578ac5c4fd7a0
Dante <quantum120@hotmail.com>
2020-04-12 13:34:19 -0500

    This program i solved it use the library mean for shorter method and as second option optaining  the average first  and then  printing for the long method.

--- average_strings.py
+++ average_strings.py
@@ -13,4 +13,34 @@
     'palace'
 ]"""
 
+from statistics import mean
+
+name = input("Input your name: ")
+print("Welcome", name)
+
+words = [
+    'seat',
+    'correspond',
+    'linen',
+    'motif',
+    'hole',
+    'smell',
+    'smart',
+    'chaos',
+    'fuel',
+    'palace'
+]
+
+total = 0
+
+"""for i in words:
+    total += len(i)
+average = float(total) / float(len(words))
+print(name, f"the average length of the following list of strings is: {average:.2f}")""" #First method
+
+print(mean([len(i) for i in words])) #Shorter method
+
+
+
+
 
