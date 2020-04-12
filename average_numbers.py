fd0c30cc0bc8d001a6ae316f93a540af9b86d9d2
Dante <quantum120@hotmail.com>
2020-04-12 12:53:28 -0500

    This program i solved it using the same base as the previous program but now dividing it by the length of the string

--- average_numbers.py
+++ average_numbers.py
@@ -5,3 +5,13 @@
 #Find the average of the numbers into the following list
 #numbers = [2, 6, 9, 10, 7, 4, 1, 9]
 
+name = input("Input your name: ")
+print("Welcome", name)
+
+numbers = [2, 6, 9, 10, 7, 4, 1, 9]
+
+average = sum(numbers) / len(numbers)
+
+#print(name, f"the average of yours numbers is: {average:.2f} ") first method
+
+print(name, "the average of yours numbers is: " , average) #this is the same as the previous line only with decimal point
