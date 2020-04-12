42f99c9909c73ae09d14b9d5b520829b3ee53eac
Dante <quantum120@hotmail.com>
2020-04-12 15:33:40 -0500

    This programs i resolved it using bucles

--- unique_lists.py
+++ unique_lists.py
@@ -15,3 +15,41 @@
   'bring'
 ]"""
 
+name = input("Input your name: ")
+print("Welcome", name)
+
+words = [
+  'crab',
+  'poison',
+  'contagious',
+  'simple',
+  'bring',
+  'sharp',
+  'playground',
+  'poison',
+  'communion',
+  'simple',
+  'bring'
+]
+print(words)
+
+
+alone = []
+#words_copy = list(words)
+
+"""for i in words:
+    if i not in alone:
+        alone.append(i)
+    else:
+        words.remove(i)
+
+print(words)
+print(alone)"""
+
+for i in range(len(words)-1, -1, -1):
+    if words[i] not in alone:
+        alone.append(words[i])
+    else:
+        words.remove(words[i])
+
+print(words)
