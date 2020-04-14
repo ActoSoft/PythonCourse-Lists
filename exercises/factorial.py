862a2f65490fc7c9efe9e12ad20d7be5b5b0a876
Liivii Manzano <liivii_neon@hotmail.com>
2020-04-14 08:53:57 -0500

    TAREA FACTORIAL MEDIO CORREGIDA

--- exercises/factorial.py
+++ exercises/factorial.py
@@ -1,12 +1,12 @@
-def factorial(a,b):
- if(b>0):
-  a=factorial(a,b-1)
-  a=a*b
+def factorial(result , value):
+ if(value>0):
+  result=factorial(result,value-1)
+  result=result*value
  else:
-  a=1
- return a
-b=int(input("Ingrese el factor a evaluar  \n"))
-a=1
-a=factorial(a,b)
-print ("el factorial es :" , a)
+  result=1
+ return result
+value=int(input("Ingrese el factor a evaluar  \n"))
+result=1
+result=factorial(result,value)
+print ("el factorial es :" , result)
     
